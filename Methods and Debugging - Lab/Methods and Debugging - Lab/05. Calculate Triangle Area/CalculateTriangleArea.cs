f796227616e7ee7e4a7d4af9deb7d77775669d72
1ooiL40 <valkovstoil@abv.bg>
2017-04-11 17:03:35 +0300

    Calcuate Triangle Area

---
+++ Methods and Debugging - Lab/Methods and Debugging - Lab/05. Calculate Triangle Area/CalculateTriangleArea.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _05.Calculate_Triangle_Area
+{
+    using System;
+
+    public class CalculateTriangleArea
+    {
+        public static void Main()
+        {
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var area = CalculateTheArea(width, height);
+            Console.WriteLine(area);
+        }
+
+        static double CalculateTheArea(double width, double height)
+        {
+            var result = (width * height) / 2;
+            return result;
+        }
+    }
+}
