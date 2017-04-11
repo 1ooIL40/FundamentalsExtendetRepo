0d5a03a0d634058d548b4b89b09ac01fea89126c
1ooiL40 <valkovstoil@abv.bg>
2017-04-11 17:12:21 +0300

    Math Power

---
+++ Methods and Debugging - Lab/Methods and Debugging - Lab/06. Math Power/MathPower.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _06.Math_Power
+{
+    using System;
+
+    public class MathPower
+    {
+        public static void Main()
+        {
+            var number = double.Parse(Console.ReadLine());
+            var power = int.Parse(Console.ReadLine());
+
+            var answer = PowerOneNumber(number, power);
+            Console.WriteLine(answer);
+        }
+
+        static double PowerOneNumber(double num, double degree)
+        {
+            double result = 1;
+
+            for (int i = 1; i <= degree; i++)
+            {
+                result *= num;
+            }
+            return result;
+        }
+    }
+}
