c2a8add1079670968545a0527e3b2b9fbade2fc3
1ooiL40 <valkovstoil@abv.bg>
2017-04-11 16:33:59 +0300

    Print Triangle

---
+++ Methods and Debugging - Lab/Methods and Debugging - Lab/03. Printing Triangle/PrintingTriangle.cs
@@ -0,0 +1,35 @@
+﻿
+namespace _03.Printing_Triangle
+{
+    using System;
+
+    public class PrintingTriangle
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            PrintLine(1, number);
+
+            for (int i = number - 1; i >= 0; i--)
+            {
+                PrintLine(1, i);
+            }
+
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
