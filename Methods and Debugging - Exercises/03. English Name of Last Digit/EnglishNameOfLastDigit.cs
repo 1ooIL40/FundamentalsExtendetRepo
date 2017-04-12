84500aefd8506963f10eee0135fc9103ff165813
1ooiL40 <valkovstoil@abv.bg>
2017-04-12 16:12:03 +0300

    English Name of Last Digit Method

---
+++ Methods and Debugging - Exercises/03. English Name of Last Digit/EnglishNameOfLastDigit.cs
@@ -0,0 +1,20 @@
+﻿
+namespace _03.English_Name_of_Last_Digit
+{
+    using System;
+
+    public class EnglishNameOfLastDigit
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var output = LastDigitInWords(number);
+        }
+
+        static object LastDigitInWords(int number)
+        {
+            var arreyOfWords = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        }
+    }
+}
