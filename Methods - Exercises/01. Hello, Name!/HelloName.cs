fbb3b2bffdf12013fadfd5fa4efcdc28ce126612
1ooiL40 <valkovstoil@abv.bg>
2017-04-12 16:21:08 +0300

    Hello name ; Max Digit Methods - Exercises.sln

---
+++ Methods - Exercises/01. Hello, Name!/HelloName.cs
@@ -0,0 +1,20 @@
+﻿namespace Methods_and_Debugging___Exercises
+{
+    using System;
+
+    public class HelloName
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var message = NamePrint(name);
+            Console.WriteLine(message);
+        }
+
+        static string NamePrint(string name)
+        {
+            var result = $"Hello, {name}!";
+            return result;
+        }
+    }
+}
---
+++ Methods - Exercises/02. Max Method/MaxMethod.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _02.Max_Method
+{
+    using System;
+
+    public class MaxMethod
+    {
+        public static void Main()
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var thirdNumber = int.Parse(Console.ReadLine());
+
+            var max = GetMax(firstNumber, secondNumber);
+            var result = GetMax(max, thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        static int GetMax(int firs, int second)
+        {
+            if (firs > second)
+            {
+                return firs;
+            }
+            return second;
+        }
+    }
+}
