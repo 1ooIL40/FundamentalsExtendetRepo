d559371a6b57f7ccfa05838ce44cf02f0b8877bb
1ooiL40 <valkovstoil@abv.bg>
2017-04-12 15:41:30 +0300

    Hello Name Method

---
+++ Methods and Debugging - Exercises/01. Hello, Name!/Methods and Debugging - Exercises/HelloName.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Methods_and_Debugging___Exercises
+{
+    using System;
+
+    public class HelloName
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var message = NamePrint(name);
+            Console.WriteLine(message);
+        }
+
+        static string NamePrint(string name)
+        {
+            var result = $"Hello, {name}!";
+            return result;
+        }
+    }
+}
