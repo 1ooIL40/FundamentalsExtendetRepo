cef22e98ba4e40049adeb13e8f653fb9330d3989
1ooiL40 <valkovstoil@abv.bg>
2017-04-12 17:28:49 +0300

    Somem ore Methods

---
+++ Methods - Exercises/04. Nth Digit/NthDigit.cs
@@ -0,0 +1,38 @@
+﻿
+namespace _04.Nth_Digit
+{
+    using System;
+
+    public class NthDigit
+    {
+        public static void Main()
+        {
+            var number = long.Parse(Console.ReadLine());
+            var nthDigit = int.Parse(Console.ReadLine());
+
+            var theDigit = FindNthDigit(number, nthDigit);
+
+            Console.WriteLine(theDigit);
+        }
+
+        static long FindNthDigit(long number, int nthDigit)
+        {
+            var index = 1;
+
+            while (number > 0)
+            {
+                if (index == nthDigit)
+                {
+                    number = number % 10;
+                    break;
+                }
+                else
+                {
+                    number = number / 10;
+                }
+                index++;
+            }
+            return number;
+        }
+    }
+}
