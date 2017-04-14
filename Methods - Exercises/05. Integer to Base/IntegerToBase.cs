893419397da1357ccbd30c06a65589f2e628bb5e
1ooiL40 <valkovstoil@abv.bg>
2017-04-14 15:47:49 +0300

    Integer to Base

---
+++ Methods - Exercises/05. Integer to Base/IntegerToBase.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.Integer_to_Base
+{
+    using System;
+
+    public class IntegerToBase
+    {
+        public static void Main()
+        {
+            var number = long.Parse(Console.ReadLine());
+            var toBase = int.Parse(Console.ReadLine());
+
+            var result = IntToBase(number, toBase);
+            Console.WriteLine(result);
+        }
+
+        static string IntToBase(long number, int toBase)
+        {
+            var output = string.Empty;
+            var remainder = 0;
+
+            remainder = (int)number % toBase;
+            if (number > 0)
+            {
+                output += number = number / toBase;
+            }
+            output += remainder;
+
+            return output;
+
+        }
+    }
+}
