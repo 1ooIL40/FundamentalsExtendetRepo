e33c3b8da3da9bc87eb7fb9128e3135695fefeef
1ooiL40 <valkovstoil@abv.bg>
2017-04-17 16:17:09 +0300

    Debugging Exercise Tricky Strings

---
+++ Debugging - Exercises/Debugging - Exercises/01. Debugging Exercise Tricky Strings/DebuggingExerciseTrickyStrings.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _01.Debugging_Exercise_Tricky_Strings
+{
+    using System;
+
+    public class DebuggingExerciseTrickyStrings
+    {
+        public static void Main()
+        {
+            var delimiter = Console.ReadLine();
+            var numberOfStrings = int.Parse(Console.ReadLine());
+
+            var result = string.Empty;
+            var currentString = string.Empty;
+
+            for (int i = 1; i <= numberOfStrings; i++)
+            {
+                currentString = Console.ReadLine();
+                result += currentString;
+                if (i != numberOfStrings)
+                {
+                    result += delimiter;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
