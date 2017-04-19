d49c0a655a70114f648385716d81000ce97acac6
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 19:07:09 +0300

    Count of Capital Letters In Array

---
+++ Simple Arrays - Exercises/07. Count of Capital Letters in Array/CountOfCapitalLettersInArray.cs
@@ -0,0 +1,36 @@
+﻿
+namespace _07.Count_of_Capital_Letters_in_Array
+{
+    using System;
+    using System.Linq;
+
+    public class CountOfCapitalLettersInArray
+    {
+        public static void Main()
+        {
+            var arrayOfStringElements = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var len = arrayOfStringElements.Length;
+            var count = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                try
+                {
+                    char chars = char.Parse(arrayOfStringElements[i]);
+                    if (chars >= 65 && chars <= 90)
+                    {
+                        count++;
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
