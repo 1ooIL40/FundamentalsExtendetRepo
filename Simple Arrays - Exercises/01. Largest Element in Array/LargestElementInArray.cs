f7fe4441b67b04f6e1720d19fb19ffa9de1f3aa3
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 17:54:22 +0300

    Largest Element in Array

---
+++ Simple Arrays - Exercises/01. Largest Element in Array/LargestElementInArray.cs
@@ -0,0 +1,27 @@
+﻿
+namespace _01.Largest_Element_in_Array
+{
+    using System;
+    using System.Linq;
+
+    public class LargestElementInArray
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var biggestElement = int.MinValue;
+            var arrayOfIntegers = new int[number];
+
+            for (int i = 0; i < number; i++)
+            {
+                arrayOfIntegers[i] = int.Parse(Console.ReadLine());
+                if (biggestElement < arrayOfIntegers[i])
+                {
+                    biggestElement = arrayOfIntegers[i];
+                }
+            }
+            Console.WriteLine(biggestElement);
+        }
+    }
+}
