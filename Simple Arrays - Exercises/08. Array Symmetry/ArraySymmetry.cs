6c6ecb08c0de0b4b9fbb6ae62ffda418e7afe8ae
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 19:48:22 +0300

    Array Symmetry

---
+++ Simple Arrays - Exercises/08. Array Symmetry/ArraySymmetry.cs
@@ -0,0 +1,43 @@
+﻿
+namespace _08.Array_Symmetry
+{
+    using System;
+    using System.Linq;
+
+    public class ArraySymmetry
+    {
+        public static void Main()
+        {
+            var arrayOfStrigs = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var len = arrayOfStrigs.Length;
+            var isTrue = false;
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                string first = arrayOfStrigs[i];
+                string second = arrayOfStrigs[len - 1 - i];
+
+                if (first == second)
+                {
+                    isTrue = true;
+                }
+                else
+                {
+                    break;  
+                }
+            }
+            if (isTrue)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
