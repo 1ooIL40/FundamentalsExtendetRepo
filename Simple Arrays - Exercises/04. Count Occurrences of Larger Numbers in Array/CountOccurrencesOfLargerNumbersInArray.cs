f167392c95a00b5fc492c1ded30a7904f25b71ea
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 18:33:02 +0300

    Count Occurrence of Larger Numbers in Array

---
+++ Simple Arrays - Exercises/04. Count Occurrences of Larger Numbers in Array/CountOccurrencesOfLargerNumbersInArray.cs
@@ -0,0 +1,31 @@
+﻿
+namespace _04.Count_Occurrences_of_Larger_Numbers_in_Array
+{
+    using System;
+    using System.Linq;
+
+    public class CountOccurrencesOfLargerNumbersInArray
+    {
+        public static void Main()
+        {
+            var arrayOfRealNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            var number = double.Parse(Console.ReadLine());
+            var count = 0;
+
+            for (int i = 0; i < arrayOfRealNumbers.Length; i++)
+            {
+                if(arrayOfRealNumbers[i] > number)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
