953636105d4b267a301df1a37f07c960167c29bd
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 18:26:10 +0300

    Count of Given Element in Array

---
+++ Simple Arrays - Exercises/03. Count of Given Element in Array/CountOfGivenElementInArray.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _03.Count_of_Given_Element_in_Array
+{
+    using System;
+    using System.Linq;
+
+    public class CountOfGivenElementInArray
+    {
+        public static void Main()
+        {
+            var arrayOfIntegers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var number = int.Parse(Console.ReadLine());
+            var count = 0;
+
+            for (int i = 0; i < arrayOfIntegers.Length; i++)
+            {
+                if (arrayOfIntegers[i] == number)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
