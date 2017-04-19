e9527bc9f11da073187bf3cb0232754f94e280dc
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 15:49:09 +0300

    Sum Array Elements

---
+++ Arrays - Simple Array Processing - Lab/01. Sum Array Elements/SumArrayElements.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _01.Sum_Array_Elements
+{
+    using System;
+    using System.Linq;
+
+    public class SumArrayElements
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var arrayOfNumbers = new int[number];
+            var sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                var numberInArray = int.Parse(Console.ReadLine());
+                arrayOfNumbers[i] = numberInArray;
+                sum += arrayOfNumbers[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Arrays - Simple Array Processing - Lab/Demo/Demo.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Demo
+{
+    using System;
+    using System.Linq;
+
+    public class Demo
+    {
+        public static void Main(string[] args)
+        {
+            // 1 2 3 4 5 6 7
+            //var text = Console.ReadLine()
+            //    .Split(' ')
+            //    .Select(int.Parse)
+            //    .ToArray();
+
+            //var text = "1 2 3 4 5 6  7 ";
+            ////For spliting the extra spaces
+            //var parts = text.Split(
+            //    new char[] { ' ' },
+            //    StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToArray();
+
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
