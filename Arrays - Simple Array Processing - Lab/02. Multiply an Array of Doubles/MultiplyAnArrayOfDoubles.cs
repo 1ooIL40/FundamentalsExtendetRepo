65683f38d3d29a4408c671f6a82cb470dbcb3dec
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 16:03:04 +0300

    Multiply an Array of Double

---
+++ Arrays - Simple Array Processing - Lab/02. Multiply an Array of Doubles/MultiplyAnArrayOfDoubles.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _02.Multiply_an_Array_of_Doubles
+{
+    using System;
+    using System.Linq;
+
+    public class MultiplyAnArrayOfDoubles
+    {
+        public static void Main()
+        {
+            var arrayToMultiply = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            var numberTpMultyply = double.Parse(Console.ReadLine());
+            var result = 0.0;
+
+            for (int i = 0; i < arrayToMultiply.Length; i++)
+            {
+                result = arrayToMultiply[i] * numberTpMultyply;
+                Console.Write($"{result} ");
+            }
+        }
+    }
+}
