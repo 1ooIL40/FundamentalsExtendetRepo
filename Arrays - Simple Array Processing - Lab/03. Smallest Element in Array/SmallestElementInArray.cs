c16a610c3c34545ac24091961573b6539bde73c8
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 16:09:28 +0300

    Smallest Element in Array

---
+++ Arrays - Simple Array Processing - Lab/03. Smallest Element in Array/SmallestElementInArray.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _03.Smallest_Element_in_Array
+{
+    using System;
+    using System.Linq;
+
+    public class SmallestElementInArray
+    {
+        public static void Main()
+        {
+            var givenArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var smallest = int.MaxValue;
+
+            for (int i = 0; i < givenArray.Length; i++)
+            {
+                if (smallest > givenArray[i])
+                {
+                    smallest = givenArray[i];
+                }
+            }
+            Console.WriteLine(smallest);
+        }
+    }
+}
