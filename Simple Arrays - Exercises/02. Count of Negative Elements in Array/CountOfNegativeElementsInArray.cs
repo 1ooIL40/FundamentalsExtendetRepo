e689a7bd3dab222292c8bc325334b94589c7f60c
1ooiL40 <valkovstoil@abv.bg>
2017-04-19 18:19:45 +0300

    Count of Negative Elements in Array

---
+++ Simple Arrays - Exercises/02. Count of Negative Elements in Array/CountOfNegativeElementsInArray.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _02.Count_of_Negative_Elements_in_Array
+{
+    using System;
+    using System.Linq;
+
+    public class CountOfNegativeElementsInArray
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var arrayOfIntegers = new int[n];
+            var count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                arrayOfIntegers[i] = int.Parse(Console.ReadLine());
+
+                if(arrayOfIntegers[i] < 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
