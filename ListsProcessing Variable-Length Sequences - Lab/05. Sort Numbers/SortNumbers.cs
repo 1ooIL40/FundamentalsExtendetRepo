edf63d21fe11004dc476a9576565f2b8f1f1fd16
1ooiL40 <valkovstoil@abv.bg>
2017-04-27 20:02:09 +0300

    commit again

---
+++ ListsProcessing Variable-Length Sequences - Lab/05. Sort Numbers/SortNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.Sort_Numbers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class SortNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            numbers.Sort();
+
+            Console.WriteLine(string.Join(" <= ", numbers));
+        }
+    }
+}
---
+++ ListsProcessing Variable-Length Sequences - Lab/06. Square Numbers/SquareNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.Square_Numbers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class SquareNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var result = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var square = Math.Sqrt(numbers[i]);
+                if (square == (int)square)
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+
+            result.Sort();
+            result.Reverse();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
