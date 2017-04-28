d0f2f5cd775fe853cda7f9eedb17494569c5c30e
1ooiL40 <valkovstoil@abv.bg>
2017-04-28 16:04:59 +0300

    Remove Elements at Odd Positions

---
+++ Lists - Exercises/01. Remove Elements at Odd Positions/RemoveElementsAtOddPositions.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.Remove_Elements_at_Odd_Positions
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class RemoveElementsAtOddPositions
+    {
+        public static void Main()
+        {
+            //var text = "First Soft ccc Uni fifth";
+            var input = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var result = new List<string>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    result.Add(input[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join("", result));
+        }
+    }
+}
