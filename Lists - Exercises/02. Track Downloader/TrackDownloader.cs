ef1bf407e171cad2ca7181a3624f21b112f541d9
1ooiL40 <valkovstoil@abv.bg>
2017-04-28 17:43:58 +0300

    Track Download

---
+++ Lists - Exercises/02. Track Downloader/TrackDownloader.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.Track_Downloader
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class TrackDownloader
+    {
+        public static void Main()
+        {
+            var blackListedWords = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var fileNames = Console.ReadLine();
+
+            var result = new List<string>();
+
+            while (fileNames != "end")
+            {
+                result.Add(fileNames);
+
+                for (int j = 0; j < blackListedWords.Count; j++)
+                {
+                    if (fileNames.Contains(blackListedWords[j]))
+                    {
+                        result.RemoveAt(result.Count - 1);
+                        break;
+                    }
+                }
+
+                fileNames = Console.ReadLine();
+            }
+            result.Sort();
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+}
