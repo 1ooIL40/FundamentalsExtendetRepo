abd3a970c18352af5e57a33b10923815b4ec4ce7
1ooiL40 <valkovstoil@abv.bg>
2017-04-29 17:39:23 +0300

    Stuck Zipper

---
+++ Lists - Exercises/06. Stuck Zipper/StuckZipper.cs
@@ -0,0 +1,108 @@
+﻿namespace _06.Stuck_Zipper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StuckZipper
+    {
+        public static void Main()
+        {
+            var textOne = "2 12 4 3 9";
+            var textTwo = "22 1 3 88 7";
+
+            var inputListOne = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var inputListTwo = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            var smallestDigit = LenghtToCompare(inputListOne, inputListTwo);
+
+            var result = ListCompareAndZip(inputListOne, inputListTwo, smallestDigit);
+
+            Console.WriteLine(smallestDigit);
+            Console.WriteLine(string.Join(" ",result));
+        }
+
+        static List<int> ListCompareAndZip(List<int> inputListOne, List<int> inputListTwo, int smallestDigit)
+        {
+            var firstList = new List<int>();
+            var secondList = new List<int>();
+            var resultList = new List<int>();
+
+            foreach (var number in inputListOne)
+            {
+                var stringNumber = Math.Abs(number).ToString();
+
+                if(stringNumber.Length == smallestDigit)
+                {
+                    firstList.Add(number);
+                }
+            }
+
+            foreach (var number in inputListTwo)
+            {
+                var stringNumber = Math.Abs(number).ToString();
+
+                if (stringNumber.Length == smallestDigit)
+                {
+                    secondList.Add(number);
+                }
+            }
+
+            var len = firstList.Count + secondList.Count;
+
+            for (int i = 0; i < len; i++)
+            {
+                var firstListNumber = 0;
+                var secondListNumber = 0;
+
+                if (secondList.Count > i)
+                {
+                    secondListNumber = secondList[i];
+                    resultList.Add(secondListNumber);
+                }
+                if (firstList.Count > i)
+                {
+                    firstListNumber = firstList[i];
+                    resultList.Add(firstListNumber);
+                }
+            }
+            
+
+            return resultList;
+        }
+
+        static int LenghtToCompare(List<int> inputListOne, List<int> inputListTwo)
+        {
+            var digit = int.MaxValue;
+
+            foreach (var number in inputListOne)
+            {
+                var stringNumber = Math.Abs(number).ToString();
+
+                if (stringNumber.Length < digit)
+                {
+                    digit = stringNumber.Length;
+                }
+            }
+            foreach (var number in inputListTwo)
+            {
+                var stringNumber = Math.Abs(number).ToString();
+
+                if (stringNumber.Length < digit)
+                {
+                    digit = stringNumber.Length;
+                }
+            }
+
+            return digit;
+        }
+    }
+}
