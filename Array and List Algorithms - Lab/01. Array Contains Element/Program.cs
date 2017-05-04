d9ade47ccec7a8124a56fd040f9e1b0989a6e5f0
1ooiL40 <valkovstoil@abv.bg>
2017-05-04 16:41:37 +0300

    Array and List Algorithms Array Contains Element

---
+++ Array and List Algorithms - Lab/01. Array Contains Element/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.Array_Contains_Element
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var n = int.Parse(Console.ReadLine());
+
+            var contain = number.Contains(n);
+            
+            if (contain)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
