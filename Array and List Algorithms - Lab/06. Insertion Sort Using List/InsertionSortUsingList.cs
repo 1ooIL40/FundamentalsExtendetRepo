02e903c48912ea6dae424e986e88331a65488424
1ooiL40 <valkovstoil@abv.bg>
2017-05-04 20:14:20 +0300

    done

---
+++ Array and List Algorithms - Lab/06. Insertion Sort Using List/InsertionSortUsingList.cs
@@ -0,0 +1,22 @@
+﻿namespace _06.Insertion_Sort_Using_List
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class InsertionSortUsingList
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.Sort();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Array and List Algorithms - Lab/07. Largest N Elements/LargestNElements.cs
@@ -0,0 +1,26 @@
+﻿namespace _06.Insertion_Sort_Using_List
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class InsertionSortUsingList
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var n = int.Parse(Console.ReadLine());
+
+            numbers.Sort();
+            numbers.Reverse();
+
+            var result = numbers.Take(n);
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
