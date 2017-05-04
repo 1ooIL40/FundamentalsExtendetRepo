937412b6e175cd60c50a68e045a54d01d6927f63
1ooiL40 <valkovstoil@abv.bg>
2017-05-04 17:20:54 +0300

    Reverse Array in Place

---
+++ Array and List Algorithms - Lab/02. Smallest Element in Array/SmallestElementInArray.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.Smallest_Element_in_Array
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SmallestElementInArray
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                  .Split(new char[] { ' ' },
+                  StringSplitOptions.RemoveEmptyEntries)
+                  .Select(int.Parse)
+                  .ToArray();
+
+            var smallestNumber = numbers.Min();
+
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
---
+++ Array and List Algorithms - Lab/03. Reverse Array In-place/ReverseArrayInPlace.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.Reverse_Array_In_place
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ReverseArrayInPlace
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+           
+            Console.WriteLine(string.Join(" ", numbers.Reverse()));
+        }
+    }
+}
