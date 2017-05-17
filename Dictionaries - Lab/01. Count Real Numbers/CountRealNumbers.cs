7c88c851a5de51f07db2b854063cdc9329eddad1
1ooiL40 <valkovstoil@abv.bg>
2017-05-17 18:50:48 +0300

    Count Real Numbers

---
+++ Dictionaries - Lab/01. Count Real Numbers/CountRealNumbers.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.Count_Real_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountRealNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            var result = new SortedDictionary<double, int>();
+
+            foreach (var num in numbers)
+            {
+                if (!result.ContainsKey(num))
+                {
+                    result[num] = 0;
+                }
+                result[num]++;
+            }
+
+            foreach (var kvp in result)
+            {
+                //var timeString = kvp.Value == 1 ? "time" : "times";
+
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value} times");
+            } 
+
+        }
+    }
+}
