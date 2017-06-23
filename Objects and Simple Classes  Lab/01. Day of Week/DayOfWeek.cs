9c69c3986393149b3c6daaad32bb7eaed8b33bda
1ooiL40 <valkovstoil@abv.bg>
2017-06-23 19:54:54 +0300

    Day of Week

---
+++ Objects and Simple Classes  Lab/01. Day of Week/DayOfWeek.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.Day_of_Week
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class DayOfWeek
+    {
+        public static void Main()
+        {
+            var date = Console.ReadLine();
+
+            var datTime = DateTime.ParseExact(date,
+                "d-M-yyyy",
+                CultureInfo.InvariantCulture);
+
+            Console.WriteLine(datTime.DayOfWeek);
+        }
+    }
+}
