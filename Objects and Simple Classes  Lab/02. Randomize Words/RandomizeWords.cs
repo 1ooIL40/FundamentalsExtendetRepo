005cf18ce31a927538318e843bf61c30de17e174
1ooiL40 <valkovstoil@abv.bg>
2017-06-23 20:02:12 +0300

    Randomized Words

---
+++ Objects and Simple Classes  Lab/02. Randomize Words/RandomizeWords.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.Randomize_Words
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RandomizeWords
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var random = new Random();
+
+            var rnd = random.Next(0, words.Count);
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                var word = words[i];
+
+                if(i != rnd)
+                {
+                    var tempWord = words[rnd];
+                    words[rnd] = word;
+                    words[i] = tempWord;
+
+                }
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
