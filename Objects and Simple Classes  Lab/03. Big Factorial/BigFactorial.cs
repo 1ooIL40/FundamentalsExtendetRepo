b43603da13db75f0a442c9132c6fc7fff4eee9cb
1ooiL40 <valkovstoil@abv.bg>
2017-06-23 20:14:13 +0300

    Big Factorial

---
+++ Objects and Simple Classes  Lab/03. Big Factorial/BigFactorial.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.Big_Factorial
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    public class BigFactorial
+    {
+
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            BigInteger result = 1;
+
+            for (int i = n; i > 1; i--)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
